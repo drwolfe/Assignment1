5880938a8832f80ac3539db251c3607a2ccd5fa9
drwolfe <31132973+drwolfe@users.noreply.github.com>
2017-08-18 20:50:54 -0400

    First commit

---
+++ src/HelloIBG.java
@@ -0,0 +1,9 @@
+
+public class HelloIBG {
+
+	public static void main(String[] args) {
+		// Print message to the console
+		System.out.println("Hello IBG!\nThank you for this opportunity!");
+	}
+
+}
